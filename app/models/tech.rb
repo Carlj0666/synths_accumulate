d92566c8d62b1baf0ba4f22fcf5050bcc2b5341e
Carlj0666 <carlj1881@yahoo.com>
2021-02-19 13:00:23 -0800

    Working with seed file, resolving user issues with create

--- app/models/tech.rb
+++ app/models/tech.rb
@@ -2,11 +2,11 @@ class Tech < ApplicationRecord
   has_many :synths
   has_many :users, through: :synths
 
-  # def synths_attributes=(attributes)
-  #   attributes.each do |number, synth_hash| 
-  #       synth = self.synths.build(synth_hash)
-  #       synth.user = current_user
-  #       synth.save
-  #   end 
+  def synths_attributes=(attributes)
+    attributes.each do |number, synth_hash| 
+        synth = self.synths.build(synth_hash)
+        synth.user = current_user
+        synth.save
+    end 
 
 end
