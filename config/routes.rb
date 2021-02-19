fecab2797b28f9ef1b500d22f188e1eb36cb7028
Carlj0666 <carlj1881@yahoo.com>
2021-02-18 18:27:51 -0800

    building out routes for views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/:provider/callback', to: 'sessions#omniauth'
+
+  get '/synths', to: 'synths#index', as: 'synths'
+  get '/synths/:id', to: 'synths#show', as: 'synth'
+  get '/synths/prodigal', to: 'synths#prodigal', as: 'prodigal_synth'
 end
--- notes.md
+++ notes.md
@@ -4,7 +4,9 @@
 1) Rails new
 2) Setup Tables/associations
 3) Follow week 6 videos to setup User Model
-4) Go back to 2nd video of week 1 and follow sequentially from there
+4) Go back to 2nd video of week 1 
+5) Finish oauth setup here: https://www.youtube.com/watch?v=AoIqSQsijN0&feature=youtu.be from: 26:43
+6) Follow sequentially from there
 
 
 # Associations Map
