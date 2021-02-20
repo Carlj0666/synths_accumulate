749afaf9f82cd7fbdf33d29039728f987e0053f0
Carlj0666 <carlj1881@yahoo.com>
2021-02-20 15:08:34 -0800

    continued work on new and show page for Tech, also adding errors messages for new Synth view

--- app/controllers/teches_controller.rb
+++ app/controllers/teches_controller.rb
@@ -1,13 +1,29 @@
 class TechesController < ApplicationController
+
   def new
+    @tech = Tech.new
   end
 
   def create
+    @tech = Tech.new(tech_params)
+    if @tech.save
+      redirect_to tech_path(@tech)
+    else
+      render :new
+    end
   end
 
   def show
+    @tech = Tech.find(params[:id])
   end
 
   def index
+    @teches = Tech.all 
+  end
+
+  private
+
+  def tech_params
+    params.require(:tech).permit(:name)
   end
 end
