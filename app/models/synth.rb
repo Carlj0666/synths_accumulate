b0ffd864f32f9df28e9474be0fe204852ee599e0
Carlj0666 <carlj1881@yahoo.com>
2021-02-20 15:21:34 -0800

    continuuing validation work

--- app/models/synth.rb
+++ app/models/synth.rb
@@ -2,6 +2,7 @@ class Synth < ApplicationRecord
   belongs_to :tech
   belongs_to :user
   validates :brand, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
   validates :name, presence: true, uniqueness: true
 
   def name_and_tech
