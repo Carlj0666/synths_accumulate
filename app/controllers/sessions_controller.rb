c4c947d2df8b1be996037926ba8902b09e431026
Carlj0666 <carlj1881@yahoo.com>
2021-02-21 16:54:15 -0800

    adding create action to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    user = User.find_by_mail(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Please Try Again"
+      redirect_to '/login'
+    end
+  end
+
   def omniauth
     user = User. create_from_omniauth(auth)
 
