4e867e02b948dbba74ea539f8179701c2a46fd74
Carlj0666 <carlj1881@yahoo.com>
2021-02-22 16:30:47 -0800

    lot's of trouble shooting. Successfuly added error messages to user login and create new views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :synths
   has_many :teches, through: :synths
   has_secure_password
-  validates :user, uniqueness: { scope: [:username, :email, :password], message: "User already exists"}
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 
 def self.create_from_omniauth(auth)
   User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
